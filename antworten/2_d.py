4a16dc2060952c8b2fb0b6296fd3017ec84e5c99
SamFaraz <58668654+SamFaraz@users.noreply.github.com>
2020-05-19 10:26:11 +0200

    Update 2_d.py

--- antworten/2_d.py
+++ antworten/2_d.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from operator import add
 import operator
 from pyspark.sql import SQLContext
@@ -33,43 +32,3 @@ print("So viele Items haben 3 Woerter in ihrem Namen")
 print(text3)
 
 
-
-=======
-from operator import add
-import operator
-from pyspark.sql import SQLContext
-from pyspark.sql import Window
-import pyspark.sql.functions
-from pyspark import SparkContext, SparkConf
-import pyspark
-
-conf = SparkConf().setAppName("part").setMaster("local[*]")
-sc = SparkContext(conf = conf)
-
-def Func(lines):
-      
-    lines = lines.split("|") 
-    return  lines[4]
-
-def words(lines):
-      
-    lines = lines.split(" ") 
-    return  lines
-
-text = sc.textFile("part.tbl")
-text1 = text.map(Func)
-text2 = text1.map(words).collect()
-text3 = text1.count()
-# text2 = text2.reduceByKey(add).collect()
-#for line in text2:
- #   print(text2)
-
-#for line in text2:
-#    if len(line)==3:
-#        print(text2)
-print("So viele Items haben 3 Wörter in ihrem Namen")
-print(text3)
-
-
-
->>>>>>> 61e8de8d6508945376acfba2d4d9239350c14945
