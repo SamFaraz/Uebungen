abffe00a9c68bfb7800c804b8688848e10ac4344
SamFaraz <farazian@live.com>
2020-05-20 19:31:14 +0200

    update

--- antworten/3_a.py
+++ antworten/3_a.py
@@ -1,13 +1,13 @@
 from operator import add
 import operator
 from pyspark.sql import SQLContext, SparkSession
-from pyspark.sql import Window, Row 
+from pyspark.sql import Window, Row
 from pyspark.sql.functions import rand
 import pyspark.sql.functions
 from pyspark import SparkContext, SparkConf
 import pyspark
 import pyspark.sql.types
-
+from pyspark.sql.types import FloatType,DoubleType,StringType,IntegerType,StructField,StructType,DecimalType
 
 conf = SparkConf().setAppName("supplier").setMaster("local[*]")
 sc = SparkContext(conf = conf)
@@ -17,16 +17,29 @@ sqlContext = SQLContext(sc)
 d0 = sc.textFile('supplier.csv')
 
 d1 = d0.map(lambda x: x.split('|'))
-df = sqlContext.createDataFrame(d1)
+
+schema = StructType([
+        
+         StructField('S_SUPPKEY', StringType(), True),
+         StructField('S_NAME', StringType(), True),
+         StructField('S_ADDRESS', StringType(), True),
+         StructField('S_NATIONKEY', StringType(), True),
+         StructField('S_PHONE', StringType(), True),
+         StructField('S_ACCTBAL', StringType(), True),
+         StructField('S_COMMENT', StringType(), True),
+         StructField('Null', StringType(), True)
+         ])
+
+df = sqlContext.createDataFrame(d1, schema=schema)
+#df = sqlContext.createDataFrame(d1)
+
+df_sort= df.orderBy('S_ACCTBAL',ascending = True)
+
+df_sort.show()
 
 
-df_sort= df.sortBy('_6',ascending=False)
 
-df_sort.show(25, False)
-
-
-
-
+#df.withColumn("_6", df["account"].cast(FloatType())).drop("account").withColumnRenamed("_6", "account")
 
 
 
